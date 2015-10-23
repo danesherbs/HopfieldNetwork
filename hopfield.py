26bc380a8951fe6659b9e6cd68f09a4916e9523a
Dane Sherburn <dane.sherburn13@imperial.ac.uk>
2015-10-23 19:47:31 +0100

    Added evolution function. About to implement function which examines network over time.

--- hopfield.py
+++ hopfield.py
@@ -19,22 +19,47 @@ class Node():
 
 class HopfieldNetwork():
     '''
-        weights is a NxN matrix [[node_1_weights], ..., [node_N_weights]]
+        *   pattern is an array of -1 or 1 e.g. [-1, 1, -1, 1, 1]
+        *   nodes states are either 1 or -1    
+        *   weights is a NxN matrix [[node_1_weights], ..., [node_N_weights]]
     '''
 
-    def __init__(self, *nodes):
-        self.nodes = nodes  # list of nodes in network
+    def __init__(self, *pattern):
+        self.pattern = pattern  # list of nodes in network
         
         # conditions for Hopfield network
-        N = len(nodes)
+        N = len(self.pattern)
         self.weights = np.zeros(shape=(N,N), dtype=int)  # NxN matrix
         for i in range(N):
             for j in range(N):
                 if i == j:  # no pseudo edges
                     self.weights[i][i] = 0
                 else:  # symmetric weights and satisfy Hebbian rule
-                    self.weights[i][j] = nodes[i].state * nodes[j].state
-                    self.weights[j][i] = nodes[i].state * nodes[j].state
+                    self.weights[i][j] = self.pattern[i] * self.pattern[j]
+                    self.weights[j][i] = self.pattern[i] * self.pattern[j]
+
+    def __str__(self):
+        output_str = ''
+        for node in self.nodes:
+            output_str += str(node.state) + '  '
+        return output_str
+
+    def run(*input_states):
+        # current states
+        input_states = []
+        for i in range(len(input_states)):
+            self.states.append(input_states[i].state)
+
+        assert len(input_states) == len(self.pattern)
+        for generation in range(100):
+            print 
+
+
+    def next_generation():  # updates asynchronously
+        for node_num, node in enumerate(self.nodes):
+            node_weights = self.weights[node_num]
+            self.states[node_num] = self.nodes[node_num].output(weights, self.states)
+
 
 
 if __name__ == '__main__':
@@ -44,7 +69,7 @@ if __name__ == '__main__':
 
     hnet = HopfieldNetwork(node1, node2, node3)
 
-    print hnet.weights
+    print hnet
 
 
 
