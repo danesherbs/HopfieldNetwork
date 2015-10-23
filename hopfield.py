9a016f2ce2b478d28b4cbadd52e2d4e57d27cb30
Dane Sherburn <dane.sherburn13@imperial.ac.uk>
2015-10-23 20:46:43 +0100

    Finished functions. Skeptical about it's correctness. Have to reason about the code

--- hopfield.py
+++ hopfield.py
@@ -8,24 +8,29 @@ class Node():
     def __init__(self, state):
         self.state = state
 
+    def __str__(self):
+        return str(self.state)
+
     def sgn(self, num):
         if num < 0:
             return -1
         return 1  # >= 0
 
     def output(self, weights, inputs):  # sgn of field (h)
-        return self.sgn(np.dot(weights, inputs))
+        weighted_output = self.sgn(np.dot(weights, inputs))
+        self.state = weighted_output
+        return weighted_output
 
 
 class HopfieldNetwork():
     '''
-        *   pattern is the set of states (1 or -1) that the network will remember
+        *   pattern is an array of states (e.g. [1,-1,-1,1]) that the network will remember
         *   weights is a NxN matrix [[node_1_weights], ..., [node_N_weights]]
     '''
 
-    
+    def __init__(self, pattern):
 
-    def __init__(self, *pattern):
+        self.N = len(pattern)
 
         def sgn(num):
             if num < 0:
@@ -45,40 +50,41 @@ class HopfieldNetwork():
                     self.weights[i][j] = pattern[i] * pattern[j]
                     self.weights[j][i] = pattern[i] * pattern[j]
 
-    # def __str__(self):
-    #     output_str = ''
-    #     for state in pattern:
-    #         output_str += str(state) + '  '
-    #     return output_str
+    def sgn(self, num):
+        if num < 0:
+            return -1
+        return 1  # >= 0
 
-    def run(*input_states):
-        
-        # initialise states
-        input_states = []
-        for i in range(len(input_states)):
-            self.states.append(input_states[i].state)
-
-        # see how network evolves over generations
-        for generation in range(100):
-            print "Hey!"
-            next_generation()
-
-
-    def next_generation():  # updates asynchronously
-        for node_num, node in enumerate(self.nodes):
+    def next_generation(self, states, nodes):  # updates asynchronously
+        for node_num, node in enumerate(nodes):
+            print states
             node_weights = self.weights[node_num]
-            self.states[node_num] = self.nodes[node_num].output(weights, self.states)
+            states[node_num] = nodes[node_num].output(node_weights, states)
+        return states, nodes
 
+    def run(self):
+        # generate a random array of states (size N)
+        states = map(lambda x: self.sgn(x), np.random.randint(-2, 2, size=self.N))
+
+        # construct list of Nodes with random states
+        nodes = []
+        for state in states:
+            nodes.append(Node(state))
+
+        # print "NODES: ",
+        # for node in nodes:
+        #     print node,
+        # print
+        
+        # see how network evolves over generations
+        for generation in range(5):
+            states, nodes = self.next_generation(states, nodes)
 
 
 if __name__ == '__main__':
-    node1 = Node(1)
-    node2 = Node(-1)
-    node3 = Node(1)
-
-    hnet = HopfieldNetwork([1,1,1])
-
-    print hnet
+    
+    hnet = HopfieldNetwork([1]*40)
+    hnet.run()
 
 
 
